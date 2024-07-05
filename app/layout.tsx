fcb3272b27a31caf8ee61a84ec2a463eb8e09ec3
Marcus <marcus@totum.io>
2024-07-05 09:51:33 -0700

    Updated title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Connected Coworking",
+  description: "Member portal for Connected Coworking",
 };
 
 export default function RootLayout({
